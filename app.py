314898013a4a6aea9cb3c6ca5e65c3f3bc40fdef
Beatriz Gonzales <blgonzalescastro@gmail.com>
2026-03-24 17:34:13 -0500

    configurando variables de entorno

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from decouple import config
 
 app = Flask(__name__)
 
 #### CONFIGURACION DE SQLALCHEMY ####
 app.app_context().push()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/demo'
+app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE-URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
